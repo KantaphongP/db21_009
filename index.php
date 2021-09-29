570fb161306ee9ffb7ddcd5ea4604ed289a72c16
KantaphongP <kantaphong.p@ku.th>
2021-09-29 11:16:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,11 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 }else{
     $controller = 'pages'
 }
+<html>
+<head></head>
+<body>
+    <?php echo "controller = ".$controller." , action ".$action.;?>
+    <br>[<a href = "">Home</a>]<br>
+    <?php require_once("routes");?>
+</body>
+</html>
