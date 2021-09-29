efba4b13f0f7a0f76ceee45b9f56cf1ad794867d
KantaphongP <kantaphong.p@ku.th>
2021-09-29 23:17:48 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -1,16 +1,22 @@
 <?php
-$servername = "localhost";
-$username = "db21_009";
-$password = "db21_009";
-$dbname = "db21_009";
+    $servername = "localhost";
+    $username = "db21_009";
+    $password = "db21_009";
+    $dbname = "db21_009";
 
-$conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername,$username,$password);
+    mysqli_set_charset($conn, "utf8");
+    if($conn->connect_error){
+        die("Connection failed:".$conn->connect_error);
+    }
+   
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if (!$conn->select_db($dbname)){
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected!!!"
+
+    if(!$conn->select_db($dbname)){
+        die("Connection failed: " . $conn->connect_error);
+  
+    }
+  
+
+
 ?>
