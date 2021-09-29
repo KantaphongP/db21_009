4c8142c473f1993c99131c4c728d13e1257327ba
KantaphongP <kantaphong.p@ku.th>
2021-09-29 21:40:07 +0700

    Create Index_order.php

---
+++ views/order/Index_order.php
@@ -0,0 +1,10 @@
+<table border = 1>
+<tr> <td>OID</td><td>date</td><td>StockID</td>
+<td>amount</td><td>status</td></tr>
+<?php foreach($order_list as $order)
+echo "<tr> <td>$order->OID</td>
+<td>$$order->date</td> <td>$$order->StockID</td>
+<td>$$order->amount</td> <td> update</td>
+<td>delete</td> </tr>"; ]
+echo "</table>";
+?>
