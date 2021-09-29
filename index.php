3e04c232db9f2af941fea85d1ad1d8a6350a31de
KantaphongP <kantaphong.p@ku.th>
2021-09-29 20:35:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,15 +10,9 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 <html>
 <head></head>
 <body>
-<<<<<<< HEAD
     controller = pages , action = home 
     <br>[<a href="/school">Home</a>]<br>
     <p> Welcome to our Homepage </p>
-=======
-    <?php echo "controller = ".$controller.",action=".$action;?>
-    <br>[<a href = "">Home</a>]<br>
-    <?php require_once("routes.php");?>
->>>>>>> e2c785f9780e3a8d94b2359bbbb2ded438d7024c
 </body>
 </html>
 
