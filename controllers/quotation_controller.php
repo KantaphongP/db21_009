fda5f26c255fb14573c2b2fb9090a5915c3b4827
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-01 21:17:09 +0700

    Update quotation_controller.php

--- controllers/quotation_controller.php
+++ controllers/quotation_controller.php
@@ -1,13 +1,7 @@
 <?php class QuotationController
 {
     public function index(){
-<<<<<<< HEAD
         $quotation_lise = Quotation :: getAll();
-        require_once('.views/quotation/index_quotation.php';
-=======
-        $quotation_list = quotation :: getAll();
-        require_once("./views/quotation/index_quotation.php");
->>>>>>> 6168389351737ef94be52887f06cd3f011de8261
-        )
+        require_once('.views/quotation/index_quotation.php');
     }
 }?>
