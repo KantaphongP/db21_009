3407aba2b8826962c6a0dfebb61291c3b78311dd
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-01 21:14:26 +0700

    Update index_quotation.php

---
+++ views/quotation/index_quotation.php
@@ -0,0 +1,24 @@
+<table border = 1>
+    <tr>
+        <td>OID</td>
+        <td>date</td>
+        <td>Staff</td>
+        <td>customer</td>
+        <td>Payment_Type</td>
+        <td>detail</td>
+        
+        
+    </tr>
+ <?php foreach($quotation_list as $quotation)
+ {
+     echo "<tr><td>$quotation->OID</td>
+     <td>$quotation->date</td>
+     <td>$quotation->Staff</td>
+     <td>$quotation->customer</td>
+     <td>$quotation->Payment_Type</td>
+     <td>$quotation->detail</td>
+     <td> update </td>
+     <td> delete </td> </tr>";
+ }
+ echo "</table>";
+ ?>
