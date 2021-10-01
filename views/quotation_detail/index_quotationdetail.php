5ac74100f6c077a52228f7ac0bc2f25f9b06bb29
KantaphongP <kantaphong.p@ku.th>
2021-10-01 21:56:12 +0700

    Update index_quotationdetail.php

--- views/quotation_detail/index_quotationdetail.php
+++ views/quotation_detail/index_quotationdetail.php
@@ -1,7 +1,7 @@
 <table border = 1>
-<tr><td>QID</td><td>DetailID</td><td>PRID</td><td>Product_Color</td><td>QTY</td><td>extra_color</td></tr>
+<tr><td>QID</td><td>DetailID</td><td>PRID</td><td>Product_Color</td><td>qty</td><td>extra_color</td></tr>
 <?php foreach($quotationdetail_list as $quotationdetail){
-    echo "<tr><td>$quotationdetail->QID</td><td>$quotationdetail->DetailID</td> <td>$quotationdetail->PRID</td><td>$quotationdetail->Product_Color</td><td> $quotationdetail->QTY</td><td> $quotationdetail->extra_color</td></tr>";    
+    echo "<tr><td>$quotationdetail->QID</td><td>$quotationdetail->DetailID</td> <td>$quotationdetail->PRID</td><td>$quotationdetail->Product_Color</td><td> $quotationdetail->qty</td><td> $quotationdetail->extra_color</td></tr>";    
 }
 echo "</table>";
 ?>
