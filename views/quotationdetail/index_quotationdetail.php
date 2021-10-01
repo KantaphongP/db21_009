2244a6c97955b0ca92252edfceaeed43621b4c9e
KantaphongP <kantaphong.p@ku.th>
2021-10-01 21:32:16 +0700

    Delete index_quotationdetail.php

--- views/quotationdetail/index_quotationdetail.php
+++
@@ -1,7 +0,0 @@
-<table border = 1>
-<tr><td>QID</td><td>DetailID</td><td>PRID</td><td>Product_Color</td><td>QTY</td><td>extra_color</td></tr>
-<?php foreach($quotationdetail_list as $quotationdetail){
-    echo "<tr><td>$quotationdetail->QID</td><td>$quotationdetail->DetailID</td> <td>$quotationdetail->PRID</td><td>$quotationdetail->Product_Color</td><td> $quotationdetail->QTY</td><td> $quotationdetail->extra_color</td></tr>";    
-}
-echo "</table>";
-?>
