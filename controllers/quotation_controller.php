e27963bc2b6c213ad76119598a2268145461477a
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-01 21:15:52 +0700

    Update quotation_controller.php

--- controllers/quotation_controller.php
+++ controllers/quotation_controller.php
@@ -1,8 +1,13 @@
 <?php class QuotationController
 {
     public function index(){
+<<<<<<< HEAD
         $quotation_lise = Quotation :: getAll();
         require_once('.views/quotation/index_quotation.php';
+=======
+        $quotation_list = quotation :: getAll();
+        require_once("./views/quotation/index_quotation.php");
+>>>>>>> 6168389351737ef94be52887f06cd3f011de8261
         )
     }
 }?>
