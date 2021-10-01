88b3bc371a264323d8d05b63bb7866620ef96d88
KantaphongP <kantaphong.p@ku.th>
2021-10-01 21:05:34 +0700

    Update pricedetail_controller.php

--- controllers/pricedetail_controller.php
+++ controllers/pricedetail_controller.php
@@ -3,6 +3,6 @@
     public function index()
     {
         $pricedetail_list = Price_detail::getAll();
-        require_once('views/price_detail/index_pricedetail.php');
+        require_once("./views/order/index_order.php");
     }
 }?>
