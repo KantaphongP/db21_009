f858c802c89e1ac00816e9006743b989155c9862
KantaphongP <kantaphong.p@ku.th>
2021-10-01 20:23:05 +0700

    Update Index_order.php

--- views/order/Index_order.php
+++ views/order/Index_order.php
@@ -1,8 +1,7 @@
 <table border = 1>
-<tr><td>OID</td><td>date</td><td>StockID</td><td>amount</td><td>status</td></tr>
+<tr><td>OID</td><td>date</td><td>StockID</td><td>order_amount</td><td>status</td><td>update</td><td>delete</td></tr>
 <?php foreach($order_list as $order){
-    echo "<tr><td>$order->OID</td><td>$$order->date</td> <td>$$order->StockID</td><td>$$order->จำนวนที่สั่ง</td><td> $order->status</td> </tr>"; 
-    
+    echo "<tr><td>$order->OID</td><td>$$order->date</td> <td>$$order->StockID</td><td>$$order->order_amount</td><td> $order->status</td> </tr>";    
 }
 echo "</table>";
 ?>
