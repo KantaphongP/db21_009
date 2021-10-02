4e84cbeab6358ab9ed385b4557bf1d1baa965aa8
KantaphongP <kantaphong.p@ku.th>
2021-10-02 19:38:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     <?php echo "controller = ".$controller.",action=".$action; ?>
     <br>
     [<a href="?controller=pages&action=home">Home</a>]
-    [<a href="?controller=order&action=index">Order</a>]
+    
     [<a href="?controller=quotation&action=index">quotation</a>]
     [<a href="?controller=quotationdetail&action=index">quotation_detail</a>]
     [<a href="?controller=pricedetail&action=index">price_detail</a>]
