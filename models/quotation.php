f085ddfa03000595b2ff87b844ada420869930a0
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-03 00:23:45 +0700

    Update quotation.php

--- models/quotation.php
+++ models/quotation.php
@@ -60,6 +60,33 @@ class Quotation{
         return "add success $result rows";
     }
 
+    public static function search($key)
+    {
+        require("connection_connect.php");
+        $sql = "SELECT DISTINCT Qc.QID , Qc.date , Qc.Name , Qc.CName ,Payment_Terms.Terms , Qc.detail
+        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail
+        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail
+        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
+        Where (Qc.QID LIKE '%$key' OR Qc.date LIKE '%$key' OR Qc.Name LIKE '%$key' OR Qc.CName LIKE '%$key' OR Payment_Terms.Terms LIKE '%$key' OR Qc.detail )";
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $QID =$my_row[QID];
+            $date = $my_row[date];
+            $Staff = $my_row[Name];
+            $customer =$my_row[CName];
+            $Payment_Type = $my_row[Terms];
+            $detail = $my_row[detail];
+            $quotation_list[]= new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
+
+            
+        }
+
+        require("connection_close.php");
+        return $quotation_list ;
+
+    }
+
 
 }
 ?>
