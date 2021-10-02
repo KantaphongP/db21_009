c234848a5de53d5114f0bb75d15c5c78e4acd146
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-02 20:58:24 +0700

    Create customer.php

---
+++ models/customer.php
@@ -0,0 +1,25 @@
+<?php class Customer{
+    public $CID,$CName,;
+    public function __construct($CID,$CName){
+        $this->CID = $CID;
+        $this->CName = $CName;
+        
+    }
+    public static function getAll(){
+        $CustomerList=[];
+        require("connection_connect.php");
+        $sql ="SELECT Customer.CID , Customer.CName ,Customer.Tel FROM Customer" ;
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $CID = $my_row[CID];
+            $CName = $my_row[CName];
+            
+            $CustomerList[]= new Customer($CID,$CName);
+        }
+    
+        require("connection_close.php");
+        return $CustomerList ;
+    }
+}
+?>
