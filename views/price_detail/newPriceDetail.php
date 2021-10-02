b7578a1dba6863b77da8dc9dab8baa27dc878658
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-02 19:28:14 +0700

    Create newPriceDetail.php

---
+++ views/price_detail/newPriceDetail.php
@@ -0,0 +1,11 @@
+<form method="get" action="">
+    <label>Product <select name="PRID">
+        <?php foreach($product_list as $product){echo "<option value= $product->PRID>$product->Name</option>";}?></select></label><br>
+    <label>min <input type="text" name="จำนวนเริ่มต้น" /> </label><br>
+    <label>max <input type="text" name="จำนวนสุดท้าย" /> </label><br>
+    <label>price <input type="text" name="price" /> </label><br>
+    <label>extra <input type="text" name="สกรีนเพิ่มสีละ" /> </label><br>
+    <input type="hidden" name="controller" value="price_detail"/>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addPriceDetail">Save</button>
+</form>
