7181e704badcd41e1ba61a1e4763b402b8b83fe5
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-02 16:20:34 +0700

    Update index_pricedetail.php

--- views/price_detail/index_pricedetail.php
+++ views/price_detail/index_pricedetail.php
@@ -1,7 +1,6 @@
 <table border = 1>
     <tr>
         <td>PriceDetailID</td>
-        <td>PRID</td>
         <td>Name</td>
         <td>ColorID</td>
         <td>จำนวนเริ่มต้น</td>
@@ -12,7 +11,6 @@
  <?php foreach($pricedetail_list as $price_detail)
  {
      echo "<tr><td>$price_detail->PriceDetailID</td>
-     <td>$price_detail->PRID</td>
      <td>$price_detail->Name</td>
      <td>$price_detail->ColorID</td>
      <td>$price_detail->จำนวนเริ่มต้น</td>
