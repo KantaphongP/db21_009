2168c81d34101070d1e9f9bda7721de405fc1dd2
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-02 19:28:08 +0700

    Update pricedetail.php

--- models/pricedetail.php
+++ models/pricedetail.php
@@ -1,18 +1,14 @@
 <?php
 class price_detail{
-    public $PriceDetailID;
     public $Name;
-    public $ColorID;
     public $จำนวนเริ่มต้น;
     public $จำนวนสุดท้าย;
     public $price;
     public $สกรีนเพิ่มสีละ;
 
-    public function __construct($PriceDetailID,$Name,$ColorID,$จำนวนเริ่มต้น,$จำนวนสุดท้าย,$price,$สกรีนเพิ่มสีละ)
+    public function __construct($Name,$จำนวนเริ่มต้น,$จำนวนสุดท้าย,$price,$สกรีนเพิ่มสีละ)
     {
-        $this->PriceDetailID = $PriceDetailID;
         $this->Name = $Name;
-        $this->ColorID = $ColorID;
         $this->จำนวนเริ่มต้น = $จำนวนเริ่มต้น;
         $this->จำนวนสุดท้าย = $จำนวนสุดท้าย;
         $this->price = $price;
@@ -22,19 +18,17 @@ class price_detail{
     public static function getAll(){
         $pricedetailList=[];
         require("connection_connect.php");
-        $sql ="SELECT Price_detail.PriceDetailID,Product.Name,Price_detail.ColorID,Price_detail.จำนวนเริ่มต้น,Price_detail.จำนวนสุดท้าย,Price_detail.price,Price_detail.สกรีนเพิ่มสีละ 
+        $sql ="SELECT DISTINCT Product.Name,Price_detail.จำนวนเริ่มต้น,Price_detail.จำนวนสุดท้าย,Price_detail.price,Price_detail.สกรีนเพิ่มสีละ 
         FROM Price_detail INNER JOIN Product ON Price_detail.PRID=Product.PRID" ;
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $PriceDetailID =$my_row[PriceDetailID];
             $Name = $my_row[Name];
-            $ColorID = $my_row[ColorID];
             $จำนวนเริ่มต้น =$my_row[จำนวนเริ่มต้น];
             $จำนวนสุดท้าย = $my_row[จำนวนสุดท้าย];
             $price =$my_row[price];
             $สกรีนเพิ่มสีละ = $my_row[สกรีนเพิ่มสีละ];
-            $pricedetailList[]= new price_detail($PriceDetailID,$Name,$ColorID,$จำนวนเริ่มต้น,$จำนวนสุดท้าย,$price,$สกรีนเพิ่มสีละ);
+            $pricedetailList[]= new price_detail($Name,$จำนวนเริ่มต้น,$จำนวนสุดท้าย,$price,$สกรีนเพิ่มสีละ);
         }
 
         require("connection_close.php");
