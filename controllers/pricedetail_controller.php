846e8fd8d8ae039c61710a2c745966f5a4fa137d
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-02 19:28:05 +0700

    Update pricedetail_controller.php

--- controllers/pricedetail_controller.php
+++ controllers/pricedetail_controller.php
@@ -5,4 +5,9 @@
         $pricedetail_list = price_detail::getAll();
         require_once("./views/price_detail/index_pricedetail.php");
     }
+    public function newPriceDetail()
+    {
+        $product_list = Product::getAll();
+        require_once("./views/price_detail/newPriceDetail.php");
+    }
 }?>
