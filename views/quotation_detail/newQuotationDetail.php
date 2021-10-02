ec1e33ee6881c18722e2197b777df3fa945c9f58
KantaphongP <kantaphong.p@ku.th>
2021-10-02 22:56:05 +0700

    Update newQuotationDetail.php

--- views/quotation_detail/newQuotationDetail.php
+++ views/quotation_detail/newQuotationDetail.php
@@ -4,7 +4,7 @@
         <?php foreach($quotation_list as $quotation){echo "<option value= $quotation->QID>$quotation->QID</option>";}?></select></label><br>
     <label>Product <select name="PRID">
         <?php foreach($product_list as $product){echo "<option value= $product->PRID>$product->Name</option>";}?></select></label><br>
-    <label>Product_Color <select name="Product_Color">
+    <label>ProductColor <select name="Product_Color">
         <?php foreach($productcolor_list as $productcolor){echo "<option value= $productcolor->ColorID>$productcolor->ColorName</option>";}?></select></label><br>
     <label>DetailID<input type="text" name="DetailID" /> </label><br>
     <label>qty<input type="text" name="qty" /> </label><br>
