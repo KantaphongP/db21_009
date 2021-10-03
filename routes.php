fd0fb7cfb3a62e15f78dfeddc76a0fd446146970
KantaphongP <kantaphong.p@ku.th>
2021-10-03 20:12:20 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 $controllers = array('pages'=>['home','error'],
 'quotation' =>['index','newQuotation', 'addQuotation','search'],
-'quotationdetail' =>['index','newQuotationDetail','addQuotationDetail','search'],
+'quotationdetail' =>['index','newQuotationDetail','addQuotationDetail','search','updateForm'],
 'pricedetail' =>['index','newPriceDetail','addPriceDetail','search','updateForm','update']) ; 
 
  
