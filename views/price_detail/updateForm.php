c070000e1a4c8325b823ef0a7120c15cb7e77c3d
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-03 19:41:45 +0700

    Update updateForm.php

--- views/price_detail/updateForm.php
+++ views/price_detail/updateForm.php
@@ -1,4 +1,6 @@
 <form method="get" action="">
+    <label>ID <input type="text" name="PriceDetailID"
+            value="<?php echo $price_detail->PriceDetailID; ?>" /> </label><br>
     <label>Product <select name="PRID">
         <?php foreach($product_list as $product)
             {echo "<option value= $product->PRID";
@@ -7,14 +9,15 @@
             }?>
         </select></label><br>  
     <label>min <input type="text" name="จำนวนเริ่มต้น"
-            value="<?php echo $price_detail->จำนวนเริ่มต้น ?>" /> </label><br>
+            value="<?php echo $price_detail->จำนวนเริ่มต้น; ?>" /> </label><br>
     <label>max <input type="text" name="จำนวนสุดท้าย"
-            value="<?php echo $price_detail->จำนวนสุดท้าย ?>" /> </label><br>
+            value="<?php echo $price_detail->จำนวนสุดท้าย; ?>" /> </label><br>
     <label>price <input type="text" name="price" 
-            value="<?php echo $price_detail->price ?>" /> </label><br> 
+            value="<?php echo $price_detail->price; ?>" /> </label><br> 
     <label>extra <input type="text" name="สกรีนเพิ่มสีละ" 
-            value="<?php echo $price_detail->สกรีนเพิ่มสีละ ?>" /> </label><br>
+            value="<?php echo $price_detail->สกรีนเพิ่มสีละ; ?>" /> </label><br>
     <input type="hidden" name="controller" value="pricedetail"/>
+
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="update">update</button>
 </form>
