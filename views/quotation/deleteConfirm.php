5e989f20ffa9645155a625e3189951d773cf07fe
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-03 23:34:47 +0700

    Create deleteConfirm.php

---
+++ views/quotation/deleteConfirm.php
@@ -0,0 +1,8 @@
+<?php echo "<br>Are you sure to delete this Quotation? <br>
+            <br> $quotation->QID $quotation->customer $quotation->Name <br>";?>
+<form method="get" action="">
+    <input type="hidden" name="controller" value="quotation"/>
+    <input type="hidden" name="QID" value="<?php echo $quotation->QID;?>" />
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="delete">delete</button>
+</form>
