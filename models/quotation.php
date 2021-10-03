d1b801d9b8b7645d7ce6450dd7fa143dead35279
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-03 21:38:52 +0700

    Update quotation.php

--- models/quotation.php
+++ models/quotation.php
@@ -20,18 +20,18 @@ class Quotation{
     public static function getAll(){
         $quotation_list=[];
         require("connection_connect.php");
-        $sql = "SELECT DISTINCT Qc.QID , Qc.date , Qc.Name , Qc.CName ,Payment_Terms.Terms , Qc.detail
-        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail
-        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail
-        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer 
-        ON Q.customer = Customer.CID) AS Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms" ;
+        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
+        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
+        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
+        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
+        Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms" ;
         /*$sql ="select * from Quotation" ;*/
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
             $QID =$my_row[QID];
-            $date = $my_row[date];
-            $Staff = $my_row[Name];
+            $date = $my_row[date_order];
+            $Staff = $my_row[Name_order];
             $customer =$my_row[CName];
             $Payment_Type = $my_row[Terms];
             $detail = $my_row[detail];
@@ -47,25 +47,43 @@ class Quotation{
     {
         
         require("connection_connect.php");
-        $sql ="INSERT INTO Quotation(QID,date,Staff,customer,Payment_Type,detail) VALUES 
+        $sql ="INSERT INTO Quotation(QID,date_order,Name_order,CName,Terms,detail) VALUES 
         ('$QID','$date','$Staff','$customer','$Payment_Type','$detail')";
         $result=$conn->query($sql);
         require("connection_close.php");
         return "Add success $result rows";
     }
 
-    /*public static function get($Staff,$customer,){
+    public static function get($Staff,$customer,$Payment_Type){
 
-    }*/
+        require("connection_connect.php");
+        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
+        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
+        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
+        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
+        Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
+        where Name_order = '$Staff' AND CName = '$customer'AND Terms = '$Payment_Type' " ;
+        $result=$conn->query($sql);
+        $my_row=$result->fetch_assoc();
+        $QID =$my_row[QID];
+        $date = $my_row[date];
+        $Staff = $my_row[Name];
+        $customer =$my_row[CName];
+        $Payment_Type = $my_row[Terms];
+        $detail = $my_row[detail];
+        require("connection_close.php");
+        return new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
+
+    }
 
     public static function search($key)
     {
         require("connection_connect.php");
-        $sql = "SELECT DISTINCT Qc.QID , Qc.date , Qc.Name , Qc.CName ,Payment_Terms.Terms , Qc.detail
-        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail
-        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail
-        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer 
-        ON Q.customer = Customer.CID) AS Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
+        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
+        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
+        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
+        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
+        Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
         where (Qc.QID LIKE '%$key%' OR Qc.date LIKE '%$key%' OR Qc.Name LIKE '%$key%' OR Qc.CName LIKE '%$key%' 
         OR Payment_Terms.Terms LIKE '%$key%' OR Qc.detail LIKE '%$key%')";
         $result=$conn->query($sql);
