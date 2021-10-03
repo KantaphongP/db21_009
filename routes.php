f93a9b0980cb786f2187936b82471edf97410989
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-03 13:22:06 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 $controllers = array('pages'=>['home','error'],
 'quotation' =>['index','newQuotation', 'addQuotation','search'],
 'quotationdetail' =>['index','newQuotationDetail'],
-'pricedetail' =>['index','newPriceDetail','addPriceDetail','search']) ; 
+'pricedetail' =>['index','newPriceDetail','addPriceDetail','search','updateForm','update']) ; 
 
  
 
