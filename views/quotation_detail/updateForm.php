ee80830b3125354e80d2afebe908388e2cac3388
KantaphongP <kantaphong.p@ku.th>
2021-10-04 00:36:05 +0700

    Update updateForm.php

--- views/quotation_detail/updateForm.php
+++ views/quotation_detail/updateForm.php
@@ -30,8 +30,6 @@
     <label>extra_color<input type="text" name="extra_color" 
             value="<?php echo $quotationdetail->extra_color; ?>" /> </label><br>
     <input type="hidden" name="controller" value="quotationdetail"/>
-    <input type="hidden" name="QID" value="<?php echo $quotationdetail->QID ;?>"/>
-    <input type="hidden" name="DetailID" value="<?php echo $quotationdetail->DetailID;?>"/>
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="update">update</button>
 </form>
