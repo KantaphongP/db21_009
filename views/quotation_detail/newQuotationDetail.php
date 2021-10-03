309938696f4fb40c9a1e9985afb3c962f5b61acd
KantaphongP <kantaphong.p@ku.th>
2021-10-03 16:45:38 +0700

    Update newQuotationDetail.php

--- views/quotation_detail/newQuotationDetail.php
+++ views/quotation_detail/newQuotationDetail.php
@@ -9,7 +9,8 @@
     <label>DetailID<input type="text" name="DetailID" /> </label><br>
     <label>qty<input type="text" name="qty" /> </label><br>
     <label>extra_color<input type="text" name="extra_color" /> </label><br>
-    <input type="hidden" name="controller" value="quotationdetail"/>
+    
+    <input type="hidden" name="controller" value="quotation_detail"/>
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="addQuotationDetail">Save</button>
     
