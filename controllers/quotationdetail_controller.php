b79a23f77cdda84a6fab0ccda064d4f97cb80d75
KantaphongP <kantaphong.p@ku.th>
2021-10-03 17:29:53 +0700

    Update quotationdetail_controller.php

--- controllers/quotationdetail_controller.php
+++ controllers/quotationdetail_controller.php
@@ -14,6 +14,21 @@
     }
     public function addQuotationDetail()
     {
-        echo "add" ; 
+        //echo "add" ; 
+        $QID = $_GET['QID'];
+        $DetailID = $_GET['DetailID']; 
+        $PRID = $_GET['PRID']; 
+        $Product_Color = $_GET['Product_Color']; 
+        $qty = $_GET['qty']; 
+        $extra_color = $_GET['extra_color'];
+        QuotationDetail::Add($QID,$DetailID,$PRID,$Product_Color,$qty,$extra_color) ; 
+        QuotationDetailController::index() ; 
+    }
+
+    public function search()
+    {
+        $key = $_GET['key'];
+        $quotationdetail_list = QuotationDetail::search($key);
+        require_once("./views/quotation_detail/index_quotationdetail.php");
     }
 }?>
