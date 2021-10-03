ddc9c3df744e098c415fc3adf90706067a68ba91
KantaphongP <kantaphong.p@ku.th>
2021-10-03 16:46:46 +0700

    Update index_pricedetail.php

--- views/price_detail/index_pricedetail.php
+++ views/price_detail/index_pricedetail.php
@@ -1,12 +1,19 @@
 <table border = 1>
     new price_detail <a href="?controller=pricedetail&action=newPriceDetail">click</a><br>
+    <form method="get" action="">
+        <input type="text" name="key">
+        <input type="hidden" name="controller" value="pricedetail"/>
+        <button type="submit" name="action" value="search">Search</button>
+    </form>
     <tr>
         <td>PRID</td>
         <td>Name</td>
-        <td>จำนวนเริ่มต้น</td>
-        <td>จำนวนสุดท้าย</td>
+        <td>MIN</td>
+        <td>MAX</td>
         <td>price</td>
         <td>สกรีนเพิ่มสีละ</td>
+        <td>update</td>
+        <td>delete</td>
     </tr>
  <?php foreach($pricedetail_list as $price_detail)
  {
@@ -16,7 +23,7 @@
      <td>$price_detail->จำนวนสุดท้าย</td>
      <td>$price_detail->price</td>
      <td>$price_detail->สกรีนเพิ่มสีละ</td>
-     <td> update </td>
+     <td><a href=?controller=pricedetail&action=updateForm&PRID=$price_detail->PRID>update</a></td>
      <td> delete </td> </tr>";
  }
  echo "</table>";
