5471316439d46a8eab61f2de43ab47c5cfc5b1a2
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-04 20:43:13 +0700

    Update newQuotation.php

--- views/quotation/newQuotation.php
+++ views/quotation/newQuotation.php
@@ -3,11 +3,11 @@
     <label>QID <input type="text" name="QID" /> </label><br>
     <label>date_order <input type="text" name="date_order" /> </label><br>
     <label>Staff <select name="Staff">
-        <?php foreach($StaffList as $Staff){echo "<option value= $Staff->SID>$Staff->Name</option>";}?></select></label><br>
+        <?php foreach($staff_list as $Staff){echo "<option value= $Staff->SID>$Staff->Name</option>";}?></select></label><br>
     <label>customer <select name="customer">
-        <?php foreach($CustomerList as $customer){echo "<option value= $customer->CID>$customer->CName</option>";}?></select></label><br>
-    <label>Payment_Terms <select name="Payment_Terms">
-        <?php foreach($PaymentTermsList as $Payment_Type){echo "<option value= $Payment_Type->Terms>$Payment_Type->Terms</option>";}?></select></label><br>
+        <?php foreach($customer_list as $customer){echo "<option value= $customer->CID>$customer->CName</option>";}?></select></label><br>
+    <label>Payment_Type <select name="Payment_Type">
+        <?php foreach($paymenttype_list as $Payment_Type){echo "<option value= $Payment_Type->Payment_Type>$Payment_Type->Payment_Type</option>";}?></select></label><br>
     
     <label>detail <input type="text" name="detail" /> </label><br>
     
