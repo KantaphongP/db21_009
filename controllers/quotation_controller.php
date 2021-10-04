548f0c4c675533e8cbf0264511d28ac61ebf4235
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-04 17:39:12 +0700

    Update quotation_controller.php

--- controllers/quotation_controller.php
+++ controllers/quotation_controller.php
@@ -23,8 +23,8 @@
     {
         $QID = $_GET['QID'];
         $date = $_GET['date_order'];
-        $Staff = $_GET['Staff'];
-        $customer = $_GET['customer'];
+        $Staff = $_GET['SID'];
+        $customer = $_GET['CID'];
         $Payment_Type = $_GET['Terms'];
         $detail = $_GET['detail'];
         Quotation::Add($QID,$date_order,$Staff,$customer,$Payment_Type,$detail);
