d74adc52f3d2d0a8e74becfff568011417eb572f
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-04 16:58:03 +0700

    Update index_quotation.php

--- views/quotation/index_quotation.php
+++ views/quotation/index_quotation.php
@@ -11,7 +11,7 @@
 
     <tr>
         <td>QID</td>
-        <td>date</td>
+        <td>date_order</td>
         <td>Staff</td>
         <td>customer</td>
         <td>Payment_Type</td>
@@ -22,7 +22,7 @@
  <?php foreach($quotation_list as $quotation)
  {
      echo "<tr><td>$quotation->QID</td>
-     <td>$quotation->date</td>
+     <td>$quotation->date_order</td>
      <td>$quotation->Staff</td>
      <td>$quotation->customer</td>
      <td>$quotation->Payment_Type</td>
