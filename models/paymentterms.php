b020b38c4abb4a030ea8f0aac6f364becc982577
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-04 19:54:11 +0700

    Update paymentterms.php

--- models/paymentterms.php
+++ models/paymentterms.php
@@ -1,19 +1,19 @@
 <?php class Payment_Terms{
-    public $Terms;
+    public $Payment_Terms;
 
-    public function __construct($Terms){
-        $this->Terms = $Terms;
+    public function __construct($Payment_Terms){
+        $this->Payment_Terms = $Payment_Terms;
         
     }
     public static function getAll(){
         $PaymentTermsList=[];
         require("connection_connect.php");
-        $sql ="SELECT Payment_Terms.Terms FROM Payment_Terms" ;
+        $sql ="SELECT Payment_Terms.Payment_Terms FROM Payment_Terms" ;
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $Terms = $my_row[Terms];
-            $PaymentTermsList[]= new Payment_Terms($Terms);
+            $Payment_Terms = $my_row[Payment_Terms];
+            $PaymentTermsList[]= new Payment_Terms($Payment_Terms);
         }
     
         require("connection_close.php");
