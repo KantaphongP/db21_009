140fa024d83290dc276ac0e7977312abbc1a06a6
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-04 21:28:11 +0700

    Update paymenttype.php

--- models/paymenttype.php
+++ models/paymenttype.php
@@ -1,23 +1,23 @@
 <?php class Payment_Type{
     public $Payment_Type;
 
-    public function __construct($Payment_Types){
+    public function __construct($Payment_Type){
         $this->Payment_Type = $Payment_Type;
         
     }
     public static function getAll(){
-        $paymenttype_list=[];
+        $PaymentTypeList=[];
         require("connection_connect.php");
         $sql ="SELECT Payment_Type.Payment_Type FROM Payment_Type" ;
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
             $Payment_Type = $my_row[Payment_Type];
-            $paymenttype_list[]= new Payment_Type($Payment_Type);
+            $PaymentTypeList[]= new Payment_Type($Payment_Type);
         }
     
         require("connection_close.php");
-        return $paymenttype_list ;
+        return $PaymentTypeList ;
     }
 }
 ?>
