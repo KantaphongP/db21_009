36549ab9369051bead96c770e65e3323df17a7fa
KantaphongP <kantaphong.p@ku.th>
2021-10-04 17:43:25 +0700

    Update deleteConfirm.php

--- views/quotation_detail/deleteConfirm.php
+++ views/quotation_detail/deleteConfirm.php
@@ -3,7 +3,8 @@
 ?>
 <form method = "get" action="">
     <input type="hidden" name="controller" value="quotationdetail"/>
-    
+    <input type="hidden" name="QID" value="<?php echo $quotation_detail->QID;?>" />
+    <input type="hidden" name="DetailID" value="<?php echo $quotation_detail->DetailID;?>" />
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="delete">delete</button>
 </form>    
