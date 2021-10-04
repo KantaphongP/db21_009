74a496326b9ed9288903add8711326796cce1edd
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-04 18:57:36 +0700

    Update quotation_controller.php

--- controllers/quotation_controller.php
+++ controllers/quotation_controller.php
@@ -30,7 +30,7 @@
         $date_order = $_GET['date_order'];
         $Staff = $_GET['Staff'];
         $customer = $_GET['customer'];
-        $Payment_Type = $_GET['Terms'];
+        $Payment_Type = $_GET['Payment_Terms'];
         $detail = $_GET['detail'];
         Quotation::update($QID,$date_order,$Staff,$customer,$Payment_Type,$detail);
         QuotationController::index();
@@ -42,7 +42,7 @@
         $date_order = $_GET['date_order'];
         $Staff = $_GET['Staff'];
         $customer = $_GET['customer'];
-        $Payment_Type = $_GET['Terms'];
+        $Payment_Type = $_GET['Payment_Terms'];
         $detail = $_GET['detail'];
         Quotation::Add($QID,$date_order,$Staff,$customer,$Payment_Type,$detail);
         QuotationController::index();
