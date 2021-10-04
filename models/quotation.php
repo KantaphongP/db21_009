242fb75aa0fa418777a8b9f7035e405b425e7521
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-04 16:55:16 +0700

    Update quotation.php

--- models/quotation.php
+++ models/quotation.php
@@ -1,26 +1,48 @@
 <?php
 class Quotation{
     public $QID;
-    public $date;
+    public $date_order;
     public $Staff;
     public $customer;
     public $Payment_Type;
     public $detail;
 
-    public function __construct($QID,$date,$Staff,$customer,$Payment_Type,$detail)
+    public function __construct($QID,$date_order,$Staff,$customer,$Payment_Type,$detail)
     {
         $this->QID =  $QID;
-        $this->date = $date;
+        $this->date_order = $date_order;
         $this->Staff = $Staff;
         $this->customer = $customer;
         $this->Payment_Type = $Payment_Type;
         $this->detail = $detail;
     }
+    
+    public static function get($QID){
+
+        require("connection_connect.php");
+        $sql = "SELECT Qc.QID , Qc.date AS date_order , Qc.Name AS Staff, Qc.CName AS customer ,Payment_Terms.Terms , Qc.detail 
+        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
+        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
+        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
+        Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
+        where QID = '$QID'  " ;
+        $result=$conn->query($sql);
+        $my_row=$result->fetch_assoc();
+        $QID =$my_row[QID];
+        $date_order = $my_row[date_order];
+        $Staff = $my_row[Straff];
+        $customer =$my_row[customer];
+        $Payment_Type = $my_row[Terms];
+        $detail = $my_row[detail];
+        require("connection_close.php");
+        return new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
+
+    }
 
     public static function getAll(){
         $quotation_list=[];
         require("connection_connect.php");
-        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
+        $sql = "SELECT Qc.QID , Qc.date AS date_order , Qc.Name AS Staff, Qc.CName AS customer ,Payment_Terms.Terms , Qc.detail 
         FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
         FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
         FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
@@ -30,56 +52,24 @@ class Quotation{
         while($my_row=$result->fetch_assoc())
         {
             $QID =$my_row[QID];
-            $date = $my_row[date_order];
-            $Staff = $my_row[Name_order];
-            $customer =$my_row[CName];
+            $date_order = $my_row[date_order];
+            $Staff = $my_row[Staff];
+            $customer =$my_row[customer];
             $Payment_Type = $my_row[Terms];
             $detail = $my_row[detail];
-            $quotation_list[]= new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
+            $quotation_list[]= new Quotation($QID,$date_order,$Staff,$customer,$Payment_Type,$detail);
 
         }
 
         require("connection_close.php");
         return $quotation_list ;
     }
-
-    public static function Add($QID,$date,$Staff,$customer,$Payment_Type,$detail)
-    {
-        
-        require("connection_connect.php");
-        $sql ="INSERT INTO Quotation(QID,date_order,Name_order,CName,Terms,detail) VALUES 
-        ('$QID','$date','$Staff','$customer','$Payment_Type','$detail')";
-        $result=$conn->query($sql);
-        require("connection_close.php");
-        return "Add success $result rows";
-    }
-
-    public static function get($Staff,$customer,$Payment_Type){
-
-        require("connection_connect.php");
-        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
-        FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
-        FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
-        FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
-        Qc INNER JOIN Payment_Terms ON Qc.Payment_Type = Payment_Terms.Terms
-        where Name_order = '$Staff' AND CName = '$customer'AND Terms = '$Payment_Type' " ;
-        $result=$conn->query($sql);
-        $my_row=$result->fetch_assoc();
-        $QID =$my_row[QID];
-        $date = $my_row[date_order];
-        $Staff = $my_row[Name_order];
-        $customer =$my_row[CName];
-        $Payment_Type = $my_row[Terms];
-        $detail = $my_row[detail];
-        require("connection_close.php");
-        return new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
-
-    }
-
+    
     public static function search($key)
     {
+        $quotation_list=[];
         require("connection_connect.php");
-        $sql = "SELECT DISTINCT Qc.QID , Qc.date AS date_order , Qc.Name AS Name_order, Qc.CName ,Payment_Terms.Terms , Qc.detail 
+        $sql = "SELECT Qc.QID , Qc.date AS date_order , Qc.Name AS Staff, Qc.CName AS customer ,Payment_Terms.Terms , Qc.detail 
         FROM (SELECT Q.QID , Q.date , Q.Name , Customer.CName ,Q.Payment_type , Q.detail 
         FROM (SELECT Quotation.QID ,Quotation.date , Staff.Name ,Quotation.customer , Quotation.Payment_Type ,Quotation.detail 
         FROM Quotation INNER JOIN Staff ON Quotation.Staff =Staff.SID) AS Q INNER JOIN Customer ON Q.customer = Customer.CID) AS 
@@ -90,12 +80,12 @@ class Quotation{
         while($my_row=$result->fetch_assoc())
         {
             $QID =$my_row[QID];
-            $date = $my_row[date_order];
-            $Staff = $my_row[Name_order];
-            $customer =$my_row[CName];
+            $date_order = $my_row[date_order];
+            $Staff = $my_row[Staff];
+            $customer =$my_row[customer];
             $Payment_Type = $my_row[Terms];
             $detail = $my_row[detail];
-            $quotation_list[]= new Quotation($QID,$date,$Staff,$customer,$Payment_Type,$detail);
+            $quotation_list[]= new Quotation($QID,$date_order,$Staff,$customer,$Payment_Type,$detail);
 
             
         }
@@ -105,6 +95,42 @@ class Quotation{
 
     }
 
+
+    public static function Add($QID,$date_order,$Staff,$customer,$Payment_Type,$detail)
+    {
+        
+        require("connection_connect.php");
+        $sql ="INSERT INTO Quotation(QID,date_order,Staff,custer,Terms,detail) VALUES 
+        ('$QID','$date_order','$Staff','$customer','$Payment_Type','$detail')";
+        $result=$conn->query($sql);
+        require("connection_close.php");
+        return "Add success $result rows";
+    }
+
+    public static function update($QID,$date_order,$Staff,$customer,$Payment_Type,$detail)
+    {
+        //echo "1" ; 
+        require("connection_connect.php");
+        //echo $extra_color ; 
+        $sql = "UPDATE Quotation SET QID = '$QID' , date_order = '$date_order',Staff = '$Staff',customer = '$customer',Terms = '$Payment_Type',detail = '$detail' 
+        WHERE QID = '$QID' " ;
+        $result=$conn->query($sql);
+        //echo $extra_color ; 
+        require("connection_close.php");
+        return "update success $result row";
+    }
+
+    public static function delete($QID)
+    {
+        //echo "00000";
+        require("connection_connect.php");
+        $sql ="DELETE from Quotation WHERE QID='$QID' ";
+        $result=$conn->query($sql);
+        require("connection_close.php");
+        return "delete success $result row";
+    }
+
+    
 
 }
 ?>
